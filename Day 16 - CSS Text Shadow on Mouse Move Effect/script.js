331a64a4361c767c29869e7c3d7698f61fa3cd03
mcadamm4 <mark.mcadam4@mail.dcu.ie>
2018-09-27 07:28:41 +0000

    Some ES6 refactoring

--- Day 16 - CSS Text Shadow on Mouse Move Effect/script.js
+++ Day 16 - CSS Text Shadow on Mouse Move Effect/script.js
@@ -2,22 +2,31 @@
 "use strict";
 
 const hero = document.getElementById('hero');
+const text = document.getElementById("titleText");
+const shadowLimit = 10;
 
 function moveShadow(e) {
-    const winHeight = window.innerHeight;
-    const winWidth = window.innerWidth;
-    const x = e.clientX;
-    const y = e.clientY;
-    const center = { x: winWidth/2, y: winHeight/2 };
+    const {offsetWidth: width, offsetHeight: height} = hero;
+    let {offsetX: x, offsetY: y} = e;
 
-    const shadowX = Math.ceil((center.x - x) / 10);
-    const shadowY = Math.ceil((center.y - y) / 10);
+    if (this !== e.target) {
+        x += e.target.offsetLeft;
+        y += e.target.offsetTop;
+    }
+
+    const center = { x: width/2, y: height/2 };
+
+    const shadowX = Math.ceil((center.x - x) / (shadowLimit/2));
+    const shadowY = Math.ceil((center.y - y) / (shadowLimit/2));
     const blur = Math.abs(shadowX*shadowY)/100;
 
     const r = Math.abs(Math.ceil(((shadowX*shadowY)/255)*100));
 
     console.log(r, shadowX, shadowY, blur);
-    document.getElementById("titleText").style.textShadow = `${shadowX}px ${shadowY}px ${blur}px rgba(${r}, 0, 0, 0.1)`;
+    text.style.textShadow = `
+        ${shadowX}px ${shadowY}px 0 rgba(${r}, 0, 0, 0.1),
+        ${shadowX*-1}px ${shadowY*-1}px 0 rgba(255, 0, 255, 0.1)
+    `;
 }
 
 hero.addEventListener('mousemove', moveShadow);
